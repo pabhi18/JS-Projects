18b04e746ebc6da9f386c082df68ec4c9e1e1737
Abhinav Pratap <abhinavpratap@Abhinavs-MacBook-Air.local>
2023-03-20 23:56:29 +0530

    I've done this Project

--- Todo-List/index.js
+++ Todo-List/index.js
@@ -13,25 +13,43 @@ document.addEventListener("DOMContentLoaded", function() {
             taskDiv.classList.add("task");
 
             const checkbox = document.createElement("input");
+            checkbox.classList.add("checkbox");
             checkbox.setAttribute("type", "checkbox");
             taskDiv.appendChild(checkbox);
 
             const taskInput = document.createElement("input");
+            taskInput.classList.add("taskInput");
             taskInput.setAttribute("type", "text");
             taskInput.setAttribute("value", taskText);
+            taskInput.readOnly = true;
             taskDiv.appendChild(taskInput);
 
             const editButton = document.createElement("input");
+            editButton.classList.add("editButton");
             editButton.setAttribute("type", "submit");
             editButton.setAttribute("value", "Edit");
             taskDiv.appendChild(editButton);
 
             const deleteButton = document.createElement("input");
+            deleteButton.classList.add("deleteButton");
             deleteButton.setAttribute("type", "submit");
             deleteButton.setAttribute("value", "Delete");
             taskDiv.appendChild(deleteButton);
 
             tasks.appendChild(taskDiv);
+
+            deleteButton.addEventListener("click", function(e){
+                tasks.removeChild(taskDiv);
+            })
+            editButton.addEventListener("click", function(e){
+                taskInput.readOnly = false;
+                editButton.setAttribute("value", "Save");
+                editButton.addEventListener("click", function(e){
+                    taskInput.readOnly = true;
+                    editButton.setAttribute("value", "Edit");
+                })
+
+            })
 
         }
     });
